6a15eef0d8b5fd899410ffaafc39c02066affdd7
FabIOSr <fabio-s-ramos@hotmail.com>
2022-03-05 15:15:05 -0300

    lang pt-br reinstaled

---
+++ lang/pt-br/message.php
@@ -0,0 +1,14 @@
+<?php
+ 
+// lang/en/messages.php
+ 
+return [
+    'register' => [
+        'Already have an account?' => 'Já tem uma conta?',
+        'login' => 'Acessar'
+    ],
+    'login' => [
+        'Don\/\'t have an account yet?' => 'Não tem uma conta ainda?',
+        'Register' => 'Registrar'
+    ]
+];
